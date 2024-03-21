bb893216201a01eea956bea6ceb6edddc956eb32
Tyler McBride <imtylermcbride@gmail.com>
2024-03-21 22:27:40 +1300

    Game Working, Win message bugged

--- client/components/PokiGuess.tsx
+++ client/components/PokiGuess.tsx
@@ -1,18 +1,25 @@
 import React, { useState } from 'react'
-import { getAllPokemon, getSpecificPokemon } from '../apis/pokemon'
+import { getAllPokemon } from '../apis/pokemon'
 import { useQuery } from '@tanstack/react-query'
 import SpecificPokemon from './SpecificPokemon'
 import { SearchResult } from '../../models/pokemon'
 
 function PokiGuess() {
-  const [score, setScore] = useState(0)
-  const [lives, setLives] = useState(5)
+  const [playerScore, setPlayerScore] = useState(0)
+  const [playerLives, setPlayerLives] = useState(5)
   const [pokemon, setPokemon] = useState([
     'pikachu',
     'charmander',
     'bulbasaur',
     'squirtle',
   ])
+  const [gameState, setGameState] = useState(0)
+  const [postHighScore, setpostHighScore] = useState({
+    name: '',
+    score: 0,
+    lives: 5,
+  })
+
   const { isLoading, isError, data } = useQuery({
     queryKey: ['randomPokemon'],
     queryFn: async () => getAllPokemon(),
@@ -46,54 +53,124 @@ function PokiGuess() {
     event.preventDefault()
     const randomPokemon = chooseRandomPokemon(4, data)
     if (guessedPokemon === pokemon[0]) {
-      setScore(score + 1)
+      setPlayerScore(playerScore + 1)
     } else {
-      setLives(lives - 1)
+      setPlayerLives(playerLives - 1)
     }
     shuffleArray(placement)
     setPokemon(randomPokemon)
     checkEnd()
   }
-
+  let gameEndMessage = 'Test'
   function checkEnd() {
-    if (score === 9) {
+    if (playerScore === 9) {
+      gameEndMessage = "You've beaten the game! Great Job"
+      setGameState(1)
       console.log('you won')
-    } else if (lives === 1) {
+    } else if (playerLives === 1) {
+      gameEndMessage = "Uh Oh! You've run out of Lives"
+      setGameState(1)
       console.log('game over')
     }
   }
-
   shuffleArray(placement)
-  // setPokemon(chooseRandomPokemon(data, 4))
-  return (
-    <div>
-      <div className="highscore-container">Score: {score}</div>
-      <div className="live-container">Lives: {lives}</div>
-      <div className="prompt-container">
-        {/* <SpecificPokemon name={randomPokemon[0]} /> */}
-        <SpecificPokemon name={pokemon[0]} />
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.currentTarget
+    setpostHighScore((prev) => ({ ...prev, [name]: value }))
+  }
+
+  async function handleSubmit(event: React.FormEvent) {
+    event?.preventDefault()
+
+    setpostHighScore((prev) => ({
+      ...prev,
+      score: playerScore,
+      lives: playerLives,
+    }))
+    console.log(postHighScore)
+    // try {
+    //   const response = await fetch('/api/v1/submit', {
+    //     method: 'POST',
+    //     headers: {
+    //       'Content-Type': 'application/json',
+    //     },
+    //     body: JSON.stringify(postHighScore),
+    //   })
+
+    //   if (!response.ok) {
+    //     throw new Error('Failed to submit highscore')
+    //   }
+
+    setpostHighScore({ name: '', score: 0, lives: 5 })
+    setPlayerScore(0)
+    setPlayerLives(5)
+    setGameState(0)
+    // } catch (error) {
+    //   console.error('Error submitting highscore:', error)
+    // }
+  }
+
+  // gameStates =  Playing (0), Lost(1), Won (2).
+
+  // if statement based on gamestate
+  if (gameState === 1) {
+    return (
+      <div>
+        <h2>{gameEndMessage}</h2>
+        <p>Final Score: {playerScore}</p>
+        <form onSubmit={() => handleSubmit()}>
+          <input
+            onChange={handleChange}
+            type="text"
+            placeholder="Name"
+            id="name"
+            name="name"
+            value={postHighScore.name}
+          ></input>
+          <button>Submit Score</button>
+        </form>
       </div>
-      <img
-        className="trainer"
-        src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/d036937d-a72f-43c1-b155-bafe45d2d742/ddtk694-d14642bf-9b73-430a-99c8-2c199bacc469.png/v1/fill/w_580,h_580/ash_ketchum_v2_back_sprite_by_robloxmaster376_ddtk694-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NTgwIiwicGF0aCI6IlwvZlwvZDAzNjkzN2QtYTcyZi00M2MxLWIxNTUtYmFmZTQ1ZDJkNzQyXC9kZHRrNjk0LWQxNDY0MmJmLTliNzMtNDMwYS05OWM4LTJjMTk5YmFjYzQ2OS5wbmciLCJ3aWR0aCI6Ijw9NTgwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.3uCg4_3dUGDt9emVtJnxxIla67MO7qiJVxNWD37Kn6c"
-        alt="Player trainer"
-      />
-      <div className="button-container">
-        <button onClick={() => handleClick(event, pokemon[placement[0]])}>
-          {pokemon[placement[0]]}
-        </button>
-        <button onClick={() => handleClick(event, pokemon[placement[1]])}>
-          {pokemon[placement[1]]}
-        </button>
-        <button onClick={() => handleClick(event, pokemon[placement[2]])}>
-          {pokemon[placement[2]]}
-        </button>
-        <button onClick={() => handleClick(event, pokemon[placement[3]])}>
-          {pokemon[placement[3]]}
-        </button>
+    )
+  } else if (gameState === 0) {
+    return (
+      <div>
+        <div className="highscore-container">Score: {playerScore}</div>
+        <div className="live-container">Lives: {playerLives}</div>
+        <div className="prompt-container">
+          <SpecificPokemon name={pokemon[0]} />
+        </div>
+        <img
+          className="trainer"
+          src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/d036937d-a72f-43c1-b155-bafe45d2d742/ddtk694-d14642bf-9b73-430a-99c8-2c199bacc469.png/v1/fill/w_580,h_580/ash_ketchum_v2_back_sprite_by_robloxmaster376_ddtk694-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NTgwIiwicGF0aCI6IlwvZlwvZDAzNjkzN2QtYTcyZi00M2MxLWIxNTUtYmFmZTQ1ZDJkNzQyXC9kZHRrNjk0LWQxNDY0MmJmLTliNzMtNDMwYS05OWM4LTJjMTk5YmFjYzQ2OS5wbmciLCJ3aWR0aCI6Ijw9NTgwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.3uCg4_3dUGDt9emVtJnxxIla67MO7qiJVxNWD37Kn6c"
+          alt="Player trainer"
+        />
+        <div className="button-container">
+          <button onClick={() => handleClick(event, pokemon[placement[0]])}>
+            {pokemon[placement[0]]}
+          </button>
+          <button onClick={() => handleClick(event, pokemon[placement[1]])}>
+            {pokemon[placement[1]]}
+          </button>
+          <button onClick={() => handleClick(event, pokemon[placement[2]])}>
+            {pokemon[placement[2]]}
+          </button>
+          <button onClick={() => handleClick(event, pokemon[placement[3]])}>
+            {pokemon[placement[3]]}
+          </button>
+        </div>
       </div>
-    </div>
-  )
+    )
+  } else {
+    console.log(gameState)
+    return (
+      <div>
+        <h2>
+          There was an issue loading the game. Please contact a facilitator.
+        </h2>
+      </div>
+    )
+  }
 }
 
 export default PokiGuess
