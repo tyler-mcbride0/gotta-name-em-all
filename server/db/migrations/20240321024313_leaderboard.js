4c1a1ba873378be3418700cab310f71ec9f80be5
jolonbentley <jolon88@hotmail.com>
2024-03-21 15:58:35 +1300

    database setup

---
+++ server/db/migrations/20240321024313_leaderboard.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function up(knex) {
+  return knex.schema.createTable('leaderboard', function (table) {
+    table.increments('id')
+    table.string('name')
+    table.integer('score')
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function down(knex) {
+  return knex.schema.dropTable('leaderboard')
+}
