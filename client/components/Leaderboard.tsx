53c233f1a1496b8bee427fd1ca1d30a2a854d9ed
Matthew Nanthameechai <mnanthameechai@gmail.com>
2024-03-22 10:15:48 +1300

    dummy data displays on leaderboard

--- client/components/Leaderboard.tsx
+++ client/components/Leaderboard.tsx
@@ -1,4 +1,37 @@
 function Leaderboard() {
+  const Scores = [
+    {
+      id: 1,
+      name: 'Tyler',
+      score: 25,
+      lives: 5,
+    },
+    {
+      id: 2,
+      name: 'Matthew',
+      score: 25,
+      lives: 3,
+    },
+    {
+      id: 3,
+      name: 'Jolon',
+      score: 22,
+      lives: 0,
+    },
+    {
+      id: 4,
+      name: 'Maitri',
+      score: 15,
+      lives: 0,
+    },
+    {
+      id: 5,
+      name: 'David',
+      score: 5,
+      lives: 0,
+    },
+  ]
+
   return (
     <>
       <div>
@@ -11,36 +44,15 @@ function Leaderboard() {
           <td>HighScore</td>
           <td>Lives</td>
         </tr>
-        <tr>
-          <td>1</td>
-          <td>Matthew</td>
-          <td>123</td>
-          <td>Lives</td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>Jolon</td>
-          <td>123</td>
-          <td>Lives</td>
-        </tr>
-        <tr>
-          <td>3</td>
-          <td>Tyler</td>
-          <td>123</td>
-          <td>Lives</td>
-        </tr>
-        <tr>
-          <td>4</td>
-          <td>Maitri</td>
-          <td>123</td>
-          <td>Lives</td>
-        </tr>
-        <tr>
-          <td>5</td>
-          <td>David</td>
-          <td>123</td>
-          <td>Lives</td>
-        </tr>
+
+        {Scores.map((score) => (
+          <tr key={score.id}>
+            <td>{score.id}</td>
+            <td>{score.name}</td>
+            <td>{score.score}</td>
+            <td>{score.lives}</td>
+          </tr>
+        ))}
       </table>
     </>
   )
