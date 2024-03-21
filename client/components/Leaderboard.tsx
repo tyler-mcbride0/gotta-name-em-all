8770380cec491f7857bd797ccc23c2f5a7e4c39e
Matthew Nanthameechai <mnanthameechai@gmail.com>
2024-03-22 10:07:34 +1300

    leaderboard placeholder n css

--- client/components/Leaderboard.tsx
+++ client/components/Leaderboard.tsx
@@ -1,7 +1,47 @@
 function Leaderboard() {
   return (
     <>
-      <div>Leaderboard</div>
+      <div>
+        <h1>Leaderboard</h1>
+      </div>
+      <table>
+        <tr>
+          <td>Rank</td>
+          <td>Name</td>
+          <td>HighScore</td>
+          <td>Lives</td>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>Matthew</td>
+          <td>123</td>
+          <td>Lives</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>Jolon</td>
+          <td>123</td>
+          <td>Lives</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td>Tyler</td>
+          <td>123</td>
+          <td>Lives</td>
+        </tr>
+        <tr>
+          <td>4</td>
+          <td>Maitri</td>
+          <td>123</td>
+          <td>Lives</td>
+        </tr>
+        <tr>
+          <td>5</td>
+          <td>David</td>
+          <td>123</td>
+          <td>Lives</td>
+        </tr>
+      </table>
     </>
   )
 }
