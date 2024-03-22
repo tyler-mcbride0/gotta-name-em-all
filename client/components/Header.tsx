c211fa30328f1d00ca4cec5a8e9bd9244bd87702
jolonbentley <jolon88@hotmail.com>
2024-03-22 15:30:41 +1300

    image code refactor

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -2,7 +2,7 @@ function Header() {
   return (
     <>
       <div className="header-container">
-        <img src="../../images/Gotta-Name-em-All-3-22-2024.png" alt="" />
+        <img src="/images/Gotta-Name-em-All-3-22-2024.png" alt="" />
         {/* <h2>Gotta Name &apos;em All</h2> */}
       </div>
     </>
